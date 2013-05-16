c886e002fe1dfca69aefac113dee6bb8c0326a9b
Григорий Зарубин <grinder@tsu.tula.ru>
2013-05-16 23:40:37 +0400

    Добавил полезные в вёрстке утилиты: пакетную оптимизацию и удаление colorspace'а у png-картинок.

---
+++ utilities/remove-png-colorspace.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rem Remove colorspaces from 'files/*.png' (all files will be replaced)
+rem Use with pngcrush.exe
+
+path programs
+for /F "usebackq" %%f in (`dir /B files\*.png`) do (
+pngcrush -rem gAMA -rem cHRM -rem iCCP -rem sRGB files\%%f files\%%f.rpcs-stub
+del /F /Q files\%%f
+rename files\%%f.rpcs-stub %%f )
