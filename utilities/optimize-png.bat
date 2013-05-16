6e607e3de75a743e66ef6e1e9be07482ea531982
Grigory Zarubin <admin@craigy.ru>
2013-05-16 23:40:37 +0400

    ??????? ???????? ? ??????? ???????: ???????? ??????????? ? ???????? colorspace'? ? png-????????.

---
+++ utilities/optimize-png.bat
@@ -0,0 +1,12 @@
+@echo off
+
+rem Optimize PNG from 'files/*.png' (all files will be replaced)
+rem Use with optipng.exe and pngout.exe
+
+path programs
+for /F "usebackq" %%f in (`dir /B files\*.png`) do (
+optipng -o7 files\%%f )
+for /F "usebackq" %%f in (`dir /B files\*.png`) do (
+pngout files\%%f files\%%f.op-stub.png
+del /F /Q files\%%f
+rename files\%%f.op-stub.png %%f )
