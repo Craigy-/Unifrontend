024039cfe94f910b5308f733fbc761d3aef5725d
Grigory Zarubin <admin@craigy.ru>
2013-11-10 23:17:00 +0400

    ????? ??????? ?????? less-?????????.

--- templates/css/less-watch.bat
+++ templates/css/less-watch.bat
@@ -1,9 +1,9 @@
 @echo off
 
 rem Starts a simple file watcher for less files
-rem Use with nodejs scripts (lessc, less-watchr))
+rem Use with nodejs scripts (lessc, less-watchr)
 
 echo Less is watching for changes. Press Ctrl-C to stop.
 echo.
 cd ..
-less-watchr --path css/less --fileToCompile css/less/styles.less --outputFilename ../styles.css --options="--yui-compress --strict-math=on"
+less-watchr --path css/less --fileToCompile css/less/styles.less --outputFilename ../styles.css --options="-x --strict-math=on"
