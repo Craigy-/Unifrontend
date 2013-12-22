334c3d00aaf377a758fe48a2bc0403b0d16f5610
Григорий Зарубин <grinder@tsu.tula.ru>
2013-12-22 17:11:31 +0400

    Update template.bat.

--- template.bat
+++ template.bat
@@ -4,10 +4,9 @@ rem Create new HTML5 template based on this repository
 rem Usage: template X:\dummy.ru\html\total
 
 if "%1" EQU "" goto error
-xcopy "templates" "%1" /i /s /e /y /exclude:exclude.txt
-copy "templates\index.htm" "%1" /y
+xcopy "templates" "%~1" /i /s /e /y /exclude:exclude.txt
+copy "templates\index.htm" "%~1" /y
 exit
 
 :error
-echo You must specify the folder in which you want to copy HTML5 template.
-exit
+echo ERROR: You must specify the folder in which you want to copy HTML5 template.
