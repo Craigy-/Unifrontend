bf05e4de2c9f4ff43d36ab74ec032946139c89a4
Grigory Zarubin <admin@craigy.ru>
2015-07-25 17:33:20 +0300

    ?????? IE8!

--- templates/css/less-watch.bat
+++ templates/css/less-watch.bat
@@ -6,5 +6,5 @@ rem Use with nodejs components (less, less-plugin-clean-css, less-watchr)
 echo Less is watching for changes. Press Ctrl-C to stop.
 echo.
 cd ..
-less-watchr --path css --fileToCompile css/less/_styles.less --outputFilename ../styles.css --options="--clean-css="--compatibility=ie8 --advanced" --strict-math=on"
+less-watchr --path css --fileToCompile css/less/_styles.less --outputFilename ../styles.css --options="--clean-css="--advanced" --strict-math=on"
 exit
