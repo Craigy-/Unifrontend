5455e46393b7542623dc6c529042e6ad14ed9d19
Григорий Зарубин <grinder@tsu.tula.ru>
2015-07-25 17:33:20 +0300

    Прощай IE8!

--- templates/css/less-compile-minified.bat
+++ templates/css/less-compile-minified.bat
@@ -3,4 +3,4 @@
 rem Compiles all less files to a single minified css
 rem Use with nodejs components (less, less-plugin-clean-css)
 
-lessc less/_styles.less --clean-css="--compatibility=ie8 --advanced" --strict-math=on > styles.css
+lessc less/_styles.less --clean-css="--advanced" --strict-math=on > styles.css
