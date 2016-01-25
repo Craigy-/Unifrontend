9836af0265116983c9505fd22f52158861b4acb0
Григорий Зарубин <admin@craigy.ru>
2016-01-25 14:16:04 +0300

    Добавил зачаток обработки ошибок в формах.

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
-
+  // Обработка ошибок в формах
+  $('.form .form-error').each(function () {
+    $(this).find('.input, textarea').on('focus', function () {
+      $(this).data('wasError', true).parent().removeClass('form-error');
+    }).on('blur', function () {
+      if ($(this).data('wasError') && $(this).val() == '') {
+        $(this).parent().addClass('form-error');
+      }
+    });
+  });
 });
