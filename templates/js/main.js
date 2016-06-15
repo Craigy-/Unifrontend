60e59537d96f1b908bcc6c9de1594e1f4a9e3c1e
Григорий Зарубин <admin@craigy.ru>
2016-06-16 00:37:18 +0300

    Поддержка селекторов в обработчике ошибок.

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,10 +1,10 @@
 $(function () {
   // Обработка ошибок в формах
   $('.form .form-error').each(function () {
-    $(this).find('.input, textarea').on('focus', function () {
+    $(this).find('.input, select, textarea').on('focus', function () {
       $(this).data('wasError', true).parent().removeClass('form-error');
     }).on('blur', function () {
-      if ($(this).data('wasError') && $(this).val() == '') {
+      if ($(this).data('wasError') && ($(this).val() == '' || $(this).val() == 'default')) {
         $(this).parent().addClass('form-error');
       }
     });
