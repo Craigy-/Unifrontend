753d8070206be8d0bb822661629f445da543a90c
Григорий Зарубин <admin@craigy.ru>
2016-06-20 12:57:22 +0300

    Универсиализация заготовки для обработки ошибок в формах. Поддержка jquery.stylish-select.

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,12 +1,27 @@
 $(function () {
   // Обработка ошибок в формах
-  $('.form .form-error').each(function () {
-    $(this).find('.input, select, textarea').on('focus', function () {
-      $(this).data('wasError', true).parent().removeClass('form-error');
-    }).on('blur', function () {
-      if ($(this).data('wasError') && ($(this).val() == '' || $(this).val() == 'default')) {
-        $(this).parent().addClass('form-error');
-      }
-    });
-  });
+  var inputs = '.input, select, textarea, .SSContainerDivWrapper',
+      defaultValues = ['', 0];
+
+  var $formErrors = $('.form .form-error');
+  if ($formErrors.length) {
+    setInterval(function () {
+      $formErrors.find(inputs).each(function () {
+        var $par = $(this).hasClass('SSContainerDivWrapper') ? $(this).parent().parent() : $(this).parent(),
+            val = $(this).hasClass('SSContainerDivWrapper') ? $(this).parent().prev().val() : $(this).val(),
+            activeState = $(this).hasClass('SSContainerDivWrapper') ? $(this).is(':visible') : $(this).is(':focus');
+        if (activeState) {
+          $par.data('wasError', true);
+          $par.removeClass('form-error');
+        } else {
+          if ($par.data('wasError')) {
+            $par.removeClass('form-error');
+            if ($.inArray(val, defaultValues) != -1) {
+              $par.addClass('form-error');
+            }
+          }
+        }
+      });
+    }, 111);
+  }
 });
