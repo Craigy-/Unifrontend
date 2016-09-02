52f7b2587bfdd8c7fbf60259f6d364f83ae2802d
Grigory Zarubin <admin@craigy.ru>
2016-09-02 13:05:15 +0300

    ?????????? ????????? ????? ???????? ????? ???????????? ?? ?????? ????????. ???? ????? ???????? ? ?? ????? ?????? ???????????? ????????.

---
+++ templates/js/config.js
@@ -0,0 +1,57 @@
+// Supported plugins configuration
+var CONFIG = {
+
+  // Replace checkboxes and radio buttons
+  inputs: $(),
+
+  // Replace selectors
+  selectors: $(),
+
+  // Carousels
+  carousels: {
+    /*'#main': {
+      wrap: false,
+      scrollableItems: 1,
+      autoscrollInterval: 0, // ms
+      pagination: true,
+      touchable: window.isTouchscreen
+    }*/
+  },
+
+  // Modal windows
+  modals: {
+    /*'.presentation': {
+      node: 'presentation',
+      onHide: function () {
+        $.overlayLoader(true, {
+          node: $('#presentation-success')
+        });
+      }
+    }*/
+  },
+
+  // Popups
+  popups: {
+    /*'#cat-item-action-faq': {
+      rel: '#popup-faq',
+      posTop: {
+        value: 'under + 10',
+        auto: false
+      },
+      posLeft: {
+        value: 'center - 31',
+        auto: false
+      },
+      manipulation: true,
+      effect: false
+    }*/
+  },
+
+  // Tabs
+  tabs: {
+    /*'#product-functions': {
+      updateHash: false
+    }*/
+  }
+
+};
