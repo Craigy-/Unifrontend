5d5558683a7a2882ef61dc2b82c7890e240cbf5c
Григорий Зарубин <admin@craigy.ru>
2018-01-31 04:30:26 +0300

    Реструктуризация проекта, всё-таки удалил оставшуюся утилиту. Добавлен readme.

--- template.bat
+++ template.bat
@@ -1,12 +1,9 @@
 @echo off
 
-rem Create new HTML5 template based on this repository
-rem Usage: template X:\dummy.ru\html\total
-
 if "%1" EQU "" goto error
 xcopy "templates" "%~1" /i /s /e /y /exclude:exclude.txt
 copy "templates\index.htm" "%~1" /y
 exit
 
 :error
-echo ERROR: You must specify the folder in which you want to copy HTML5 template.
+echo ERROR: You must specify the folder in which you want to create new HTML Templates.
