65739ee919a2f03978f3f22793750489572c22dd
Craigy- <admin@craigy.ru>
2023-10-23 19:36:51 +0300

    Partial reworking of the built-in adaptability and layout.

--- app/js/src/main.js
+++ app/js/src/main.js
@@ -23,27 +23,24 @@ $(function () {
 
     // Responsive menu
     // Optional: /js/plugins/jquery.event.move.js, /js/plugins/jquery.event.swipe.js
-    $('.mobile-menu-link, nav, #mobile-menu-overlay').off('.ht');
-    if (ww < 768) {
-
-      $('.mobile-menu-link').on('click.ht', function (e) {
+    $('.mobile-menu-link').on('click.ht', function (e) {
+      e.preventDefault();
+      $(document.body).toggleClass('nav-opened');
+    });
+    $('nav').on('swipeleft.ht swiperight.ht', function () {
+      $('.mobile-menu-link').trigger('click');
+    }).on('movestart.ht', function (e) {
+      if ((e.distX > e.distY && e.distX < -e.distY) || (e.distX < e.distY && e.distX > -e.distY)) {
         e.preventDefault();
-        $(document.body).toggleClass('nav-opened');
-      });
-      $('nav').on('swipeleft.ht swiperight.ht', function () {
-        $('.mobile-menu-link').trigger('click');
-      }).on('movestart.ht', function (e) {
-        if ((e.distX > e.distY && e.distX < -e.distY) || (e.distX < e.distY && e.distX > -e.distY)) {
-          e.preventDefault();
-        }
-      });
-      $('#mobile-menu-overlay').on('click.ht', function () {
-        $(document.body).removeClass('nav-opened');
-      });
-
-    }
+      }
+    });
+    $('#mobile-menu-overlay').on('click.ht', function () {
+      $(document.body).removeClass('nav-opened');
+    });
 
     // Responsive manipulations
+    if (ww < 768) {
+    }
   }
   responsiveFixes();
   $(window).on('load ' + GLOBAL.actualResizer, responsiveFixes);
